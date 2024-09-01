77dae7efc9b504a1288dad250f286d18f46f1a51
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-01 17:58:58 +0500

    style fixed

--- components/SomeProjects.tsx
+++ components/SomeProjects.tsx
@@ -245,9 +245,11 @@ interface Project {
 
 const SomeProjects: FC = () => {
   return (
-    <section id="projects" className="py-16 bg-gray-900 text-white mt-10">
+    <section id="projects" className="py-16 bg-gray-900 text-white ">
       <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold mb-8 text-center">Projects</h2>
+        <h2 className="text-3xl font-bold mb-8 text-center">
+          Featured Projects
+        </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 space-y-4   gap-8">
           {projects.map((project) => (
             <div
